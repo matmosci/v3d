6f1ad5b4095cff5bcc71f0d5210ec604cf4fe95a
matmosci <matmosci@gmail.com>
2026-02-08 17:51:23 +0100

    chore: update dependencies in package.json
    - Added nodemailer and nuxt-mongoose to dependencies
    - Added zod to dependencies
    - Added @types/node to devDependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/icon', 'nuxt-auth-utils'],
-})
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    modules: ["@nuxt/ui", "@nuxt/icon", "nuxt-auth-utils", "nuxt-mongoose"],
+    runtimeConfig: {
+        session: {
+            password: process.env.NUXT_SESSION_PASSWORD!,
+        },
+        LOGIN_TOKEN_EXPIRATION: 1000 * 60 * 10, // 10 minutes
+        mailer: {
+            host: process.env.MAILER_HOST,
+            port: process.env.MAILER_PORT,
+            auth: {
+                user: process.env.MAILER_USER,
+                pass: process.env.MAILER_PASS,
+            },
+        },
+        contact: {
+            admin: process.env.CONTACT_ADMIN_EMAIL,
+        }
+    },
+});
