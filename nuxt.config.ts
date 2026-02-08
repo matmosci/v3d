19e10af186406730e405818a05d3f93ebf9f6c27
matmosci <matmosci@gmail.com>
2026-02-08 17:50:55 +0100

    chore: add nuxt-auth-utils as a dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/icon'],
+  modules: ['@nuxt/ui', '@nuxt/icon', 'nuxt-auth-utils'],
 })
