f3250823c9fbecb053c697f5be04fc437cd7c25b
matmosci <matmosci@gmail.com>
2026-02-12 22:27:19 +0100

    refactor: standardize model definition syntax across Asset, Instance, LoginToken, and User models

--- server/models/Asset.model.js
+++ server/models/Asset.model.js
@@ -2,18 +2,16 @@ import { defineMongooseModel } from '#nuxt/mongoose';
 import mongoose from 'mongoose';
 import crypto from 'crypto';
 
-export const AssetModel = defineMongooseModel(
-    {
-        name: "Asset",
-        schema: {
-            _id: { type: String, default: () => crypto.randomUUID() },
-            user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
-            originalname: { type: String, required: true },
-            thumbnail: { type: String, required: false },
-            size: { type: Number, required: true },
-        },
-        options: {
-            timestamps: true
-        }
+export const AssetModel = defineMongooseModel({
+    name: "Asset",
+    schema: {
+        _id: { type: String, default: () => crypto.randomUUID() },
+        user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+        originalname: { type: String, required: true },
+        thumbnail: { type: String, required: false },
+        size: { type: Number, required: true },
+    },
+    options: {
+        timestamps: true
     }
-);
+});
--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,19 +1,17 @@
 import { defineMongooseModel } from '#nuxt/mongoose';
 
-export const UserModel = defineMongooseModel(
-    {
-        name: "User",
-        schema: {
-            email: { type: String, unique: true, minlength: [5, "Min 5 znaków"], maxlength: [64, "Max 64 znaki"], match: [/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, "Niepoprawny format"] },
-            access: { type: Number, default: 0 },
-        },
-        options: {
-            timestamps: true
-        },
-        hooks(schema) {
-            schema.pre("save", function () {
-                this.email = this.email.trim().toLowerCase();
-            });
-        },
-    }
-);
+export const UserModel = defineMongooseModel({
+    name: "User",
+    schema: {
+        email: { type: String, unique: true, minlength: [5, "Min 5 znaków"], maxlength: [64, "Max 64 znaki"], match: [/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, "Niepoprawny format"] },
+        access: { type: Number, default: 0 },
+    },
+    options: {
+        timestamps: true
+    },
+    hooks(schema) {
+        schema.pre("save", function () {
+            this.email = this.email.trim().toLowerCase();
+        });
+    },
+});
