8344d25252890ce1f9cc08bf810fe1c01ae0246e
matmosci <matmosci@gmail.com>
2026-02-12 19:19:24 +0100

    feat: add AssetsItem component preview button

--- app/components/AssetsItem.vue
+++ app/components/AssetsItem.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="border border-default duration-150 rounded-lg bg-default hover:bg-elevated/25 cursor-pointer px-1 pb-1 h-48 flex flex-col select-none">
-        <div>
-            <div class="font-medium">{{ asset.originalname }}</div>
-            <div class="text-sm text-gray-400">{{ (asset.size / 1024).toFixed(2) }} KB</div>
+    <div
+        class="border border-default duration-150 rounded-lg bg-default hover:bg-elevated/25 cursor-pointer px-2 pb-1 h-48 flex flex-col select-none">
+        <div class="font-medium">{{ asset.originalname }}</div>
+        <div class="text-sm text-gray-400">{{ (asset.size / 1024).toFixed(2) }} KB</div>
+        <div class="mt-auto text-end">
+            <UButton icon="i-lucide-eye" variant="link" class="cursor-pointer"></UButton>
         </div>
     </div>
 </template>
