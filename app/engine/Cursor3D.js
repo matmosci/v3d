b3f11eddf0b258850a83a6e072bd3dd8f560043b
matmosci <matmosci@gmail.com>
2026-02-18 18:11:38 +0100

    refactor: remove commented-out code and clean up ghost object handling

--- app/engine/Cursor3D.js
+++ app/engine/Cursor3D.js
@@ -10,72 +10,6 @@ import {
     SpotLight,
 } from "three";
 
-// const whiteMaterial = new MeshBasicMaterial({ color: 0xffffff });
-// const pickedAssetMaterial = new MeshBasicMaterial({ color: 0x0088ff, opacity: 0.8, transparent: true });
-
-// const cursorObject3D = new Object3D();
-
-// const cursorRing = new Mesh(
-//     new RingGeometry(0.05, 0.06, 32),
-//     whiteMaterial
-// );
-// cursorRing.translateZ(0.01);
-// cursorRing.layers.set(1);
-
-// const cursorLine = new Mesh(
-//     new CylinderGeometry(0.005, 0.005, 0.2, 8).rotateX(Math.PI / 2),
-//     whiteMaterial
-// );
-// cursorLine.translateZ(0.099);
-// cursorLine.layers.set(1);
-
-// cursorObject3D.add(cursorRing);
-// cursorObject3D.add(cursorLine);
-// const pickedObjectGroup = new Object3D();
-// pickedObjectGroup.layers.set(1);
-// cursorObject3D.add(pickedObjectGroup);
-// pickedObjectGroup.rotation.x = Math.PI / 2;
-
-// let pickedAsset = null;
-
-// events.on("object:placement:update", ({ object }) => {
-//     setTimeout(() => {
-//         window.addEventListener("click", confirmPlacement, { once: true });
-//     });
-//     pickedObjectGroup.clear();
-//     pickedAsset = object.name;
-//     if (object) {
-//         const pickedObjectMaterialCache = new Map();
-//         events.emit("mode:change", { mode: "navigation" });
-//         pickedObjectGroup.add(object);
-//         pickedObjectGroup.traverse((child) => {
-//             child.layers.set(1);
-//             if (child.isMesh) {
-//                 console.log(child);
-//                 pickedObjectMaterialCache.set(child.uuid, child.material);
-//                 child.material = pickedAssetMaterial.clone();
-//                 child.material.map = pickedObjectMaterialCache.get(child.uuid).map;
-//             }
-//         });
-//     };
-// });
-
-// function confirmPlacement() {
-//     if (!pickedAsset) return;
-//     events.emit("object:placement:confirm", { asset: pickedAsset, matrix: pickedObjectGroup.matrixWorld.toArray() });
-//     clearPickedObject();
-// }
-
-// function clearPickedObject() {
-//     pickedObjectGroup.clear();
-//     pickedAsset = null;
-//     window.removeEventListener("click", confirmPlacement);
-// }
-
-// events.on("mode:change", ({ mode }) => {
-//     if (mode === "overlay") clearPickedObject();
-// });
-
 class Cursor3DIndicator extends Object3D {
     constructor(ctx) {
         super();
@@ -117,7 +51,6 @@ class GhostObject extends Object3D {
 
     clearObject() {
         this.clear();
-        console.log("Clearing ghost object, previous asset was:", this.pickedAsset);
         this.pickedAsset = null;
         window.removeEventListener("click", this.confirmPlacement);
     }
@@ -130,7 +63,6 @@ class GhostObject extends Object3D {
             setTimeout(() => {
                 window.addEventListener("click", this.finishPlacement.bind(this), { once: true });
             });
-            console.log("Setting ghost object to asset:", this.pickedAsset);
             this.add(object);
             this.traverse((child) => {
                 child.layers.set(1);
