e864248099eb8eee3c5f0693f9d234f4c5971b52
matmosci <matmosci@gmail.com>
2026-02-22 04:56:29 +0100

    feat: update keybinding for cycling transform mode from 'T' to 'R'

--- app/engine/Engine.js
+++ app/engine/Engine.js
@@ -57,7 +57,7 @@ export default class Engine {
         this.context.keybindings.bindAction("moveDown", ["KeyQ"]);
         this.context.keybindings.bindAction("sprint", ["ShiftLeft", "ShiftRight"]);
         this.context.keybindings.bindAction("toggleCursorLight", ["KeyF"]);
-        this.context.keybindings.bindAction("cycleTransformMode", ["KeyT"]);
+        this.context.keybindings.bindAction("cycleTransformMode", ["KeyR"]);
         this.context.keybindings.bindAction("toggleMarkersVisibility", ["KeyM"]);
         this.context.keybindings.bindAction("toggleGridHelper", ["KeyH"]);
     }
